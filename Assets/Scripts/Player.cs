f94511755e94858fab24db19ac4609138b675570
Callum Chapman <callum.chapman.10@gmail.com>
2019-07-27 16:23:11 +0100

    basic of a working game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isOnGround = false;
+    public Object player;
+    public float g = 0.0f; //set to 0 by default
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }//Start
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        transform.position = new Vector2(x + 0.032f, y + g);
+        isOnGround = (y == 2.0f);
+
+        if (Input.GetKeyDown("space") && isOnGround)
+        {
+          Jump();
+        }
+
+        //g = -1.0f;
+
+
+
+    }//Update
+
+
+
+    void Jump()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        //transform.position = new Vector2(x + 0.016f, y + 2.0f);
+        g = 0.1f;
+
+
+    }//Jump
+
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Collision detected: " + coll);
+        Destroy(player);
+    }//OnCollisionEnter2D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//Player
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public void OnMouseClick(string SceneName) //UI Buttons
+	{
+		SceneManager.LoadScene(SceneName, mode: LoadSceneMode.Single); //Loads game when menu button is pressed  
+	}
+
+	public void GameOver(){
+		SceneManager.LoadScene("GameOver", mode: LoadSceneMode.Single);
+	}
+}
